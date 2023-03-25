f43d4342052477fe94615b3705654d700124f9c2
Zoltán Benedek <bzolka@aut.bme.hu>
2023-03-25 18:10:28 +0100

    Többszálú demók

---
+++ 06 Konkurens, többszálú/02 UtilizeCPU/Range.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilizeCPU
+{
+    class Range
+    {
+        public long From;
+        public long To;
+        public Range(long from, long to)
+        {
+            From = from;
+            To = to;
+        }
+    }
+}

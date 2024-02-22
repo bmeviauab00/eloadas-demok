e1ce3c296d2d27d7ee4f61da0eaac91a03a45762
Zoltán Benedek <bzolka@aut.bme.hu>
2024-02-22 21:22:10 +0100

    LINQ első iteráció

--- 03 Nyelvi eszközök 2 (lang tools 2)/03 LINQ/Program.cs
+++ 03 Nyelvi eszközök 2 (lang tools 2)/03 LINQ/Program.cs
@@ -2,26 +2,19 @@
 List<string> fruits = new List<string>() { "apple", "grape", "peach", "banana", "pineapple" };
 
 // Filtering
-var fuits2 = fruits.Where(f => f.Length <= 5);
-// The type of fuits2 is IEnumerable<string>, we can use a
+var fruits2 = fruits.Where(f => f.Length <= 5);
+// The type of fuits2 is IEnumerable<string>, so we can use a
 // forach to enumerate all items.
-foreach (var f in fruits)
+foreach (var f in fruits2)
     Console.WriteLine(f);
 
-// ToList creates a new List from the IEnumerable<string>
-var fuits3 = fruits.Where(f => f.Length <= 5).ToList();
 
-
-var fuits4 = fruits
+var fuits3 = fruits
     .Where(f => f.Length <= 5)  // Filtering
     .OrderBy(f => f.Length)     // Sorting
     .Select(f => $"Reverse: {f.Reverse()}, Length: {f.Length}") // Projection
     .ToList();
 
 
-class StringWithLength
-{
-    public string Val;
-    public int Y;
-}
-
+// ToList creates a new List from the IEnumerable<string>
+var fuits4 = fruits.Where(f => f.Length <= 5).ToList();

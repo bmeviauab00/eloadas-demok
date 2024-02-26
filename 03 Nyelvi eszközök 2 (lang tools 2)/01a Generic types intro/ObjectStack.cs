c446be2d338abaceb978250f7e9c589f99f39b65
zbenedek <benedek.zoltan@mirango.hu>
2024-02-26 21:04:05 +0100

    ObjectStack formatting

--- 03 Nyelvi eszközök 2 (lang tools 2)/01a Generic types intro/ObjectStack.cs
+++ 03 Nyelvi eszközök 2 (lang tools 2)/01a Generic types intro/ObjectStack.cs
@@ -2,45 +2,45 @@ namespace Generic;
 
 public class Stack
 {
-	readonly int size;
-	int current = 0;
-	object[] items;
+    readonly int size;
+    int current = 0;
+    object[] items;
 
-	public Stack()
-		: this(100)
-	{ }
+    public Stack()
+        : this(100)
+    { }
 
-	public Stack( int size )
-	{
-		this.size = size;
-		items = new object[size];
-	}
+    public Stack(int size)
+    {
+        this.size = size;
+        items = new object[size];
+    }
 
-	public void Push( object item )
-	{
-		if (current >= size)
-			throw new InvalidOperationException("The stack is full.");
+    public void Push(object item)
+    {
+        if (current >= size)
+            throw new InvalidOperationException("The stack is full.");
 
-		items[current++] = item;
-	}
+        items[current++] = item;
+    }
 
-	public object Pop()
-	{
-		current--;
-		if (current >= 0)
-			return items[current];
-		else
-			throw new InvalidOperationException("The stack is empty.");
-	}
+    public object Pop()
+    {
+        current--;
+        if (current >= 0)
+            return items[current];
+        else
+            throw new InvalidOperationException("The stack is empty.");
+    }
 }
 
 class Program1
 {
-	static void Main(string[] args)
-	{
-		Stack objectStack = new Stack();
-		objectStack.Push(1);
-		objectStack.Push("sss");
-		int x = (int)objectStack.Pop();
-	}
+    static void Main(string[] args)
+    {
+        Stack objectStack = new Stack();
+        objectStack.Push(1);
+        objectStack.Push("sss");
+        int x = (int)objectStack.Pop();
+    }
 }

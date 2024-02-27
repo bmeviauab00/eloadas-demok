39ea15ebb862cbdf2b29eb4c9329627556d9f4a3
zbenedek <benedek.zoltan@mirango.hu>
2024-02-27 09:14:15 +0100

    Lambda capture

--- 03 Nyelvi eszközök 2 (lang tools 2)/02d Lambda capture/Program.cs
+++ 03 Nyelvi eszközök 2 (lang tools 2)/02d Lambda capture/Program.cs
@@ -6,24 +6,24 @@ class Program
         IncrementFactory incrementFactory = new IncrementFactory();
 
         // We get a function that can be used to increment values by 10
-        var increment1 = incrementFactory.GetIncrementFunc1();
-        int res = increment1(100);
+        var increment0 = incrementFactory.GetIncrementFunc0();
+        int res = increment0(100);
         Console.WriteLine(res);
 
         // We get a function that can be used to increment values defined by a local variable
-        var increment2 = incrementFactory.GetIncrementFunc2();
+        var increment = incrementFactory.GetIncrementFunc();
+        res = increment(100);
+        Console.WriteLine(res);
+
+        var increment2 = incrementFactory.GetIncrementFunc2(30);
         res = increment2(100);
         Console.WriteLine(res);
 
-        var increment3 = incrementFactory.GetIncrementFunc3(30);
+        var increment3 = incrementFactory.GetIncrementFunc3();
         res = increment3(100);
         Console.WriteLine(res);
-
-        var increment4 = incrementFactory.GetIncrementFunc4();
-        res = increment4(100);
-        Console.WriteLine(res);
         incrementFactory.MemberDelta = 45;
-        res = increment4(100); // This uses the updated MemberDelta value!
+        res = increment3(100); // This uses the updated MemberDelta value!
         Console.WriteLine(res);
     }
 }
@@ -33,25 +33,25 @@ class IncrementFactory
 {
     public int MemberDelta = 40;
 
-    public Func<int, int> GetIncrementFunc1()
+    public Func<int, int> GetIncrementFunc0()
     {
         return n => n + 10;
     }
 
-    public Func<int, int> GetIncrementFunc2()
+    public Func<int, int> GetIncrementFunc()
     {
         // Uses local variable
         int delta = 20;
         return n => n + delta;
     }
 
-    public Func<int, int> GetIncrementFunc3(int delta)
+    public Func<int, int> GetIncrementFunc2(int delta)
     {
         // Uses parameter
         return n => n + delta;
     }
 
-    public Func<int, int> GetIncrementFunc4()
+    public Func<int, int> GetIncrementFunc3()
     {
         // Uses member field
         return n => n + MemberDelta;
